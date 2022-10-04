4b59d170a2e22a73932d2565582064a0ab2c2f82
wolfking-413 <2155569805@qq.com>
2022-10-05 00:53:19 +0800

    push test

--- src/main/java/com/atSCY/git/GitTest.java
+++ src/main/java/com/atSCY/git/GitTest.java
@@ -12,6 +12,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("master test!");
         System.out.println("master test!");
-        System.out.println("master test!");
+        System.out.println("push test");
     }
 }
