80cf7430d932acf4a767e9a993bed8de0a53f5a0
wolfking-413 <2155569805@qq.com>
2022-10-05 09:44:48 +0800

    gitee commit the second day

--- src/main/java/com/atSCY/git/GitTest.java
+++ src/main/java/com/atSCY/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("pull test");
         System.out.println("gitee test");
         System.out.println("gitee pull test2");
+        System.out.println("gitee pull test2 第二天早上");
     }
 }
